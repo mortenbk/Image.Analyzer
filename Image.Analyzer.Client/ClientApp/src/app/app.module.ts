fb78f7431d50e32522930328bd235a89961b952f
mortenbk <morphy86@gmail.com>
2021-10-20 12:04:57 +0200

    second

--- Image.Analyzer.Client/ClientApp/src/app/app.module.ts
+++ Image.Analyzer.Client/ClientApp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ImageAnalyzerComponent } from './image-analyzer/image-analyzer.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -7,16 +8,21 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ApiModule, Configuration, ConfigurationParameters } from './openapi';
+
+export function apiConfigFactory (): Configuration {
+  const params: ConfigurationParameters = {
+    // set configuration parameters here.
+  }
+  return new Configuration(params);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    ImageAnalyzerComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,9 +30,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: 'imageanalyzer', component: ImageAnalyzerComponent },
+    ]),
+    ApiModule.forRoot(apiConfigFactory)
   ],
   providers: [],
   bootstrap: [AppComponent]
